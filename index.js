157b0c0f9658d3f5d0ace66cda39dd0302204a5d
elugo33 <140855002+elugo33@users.noreply.github.com>
2023-09-21 14:19:57 -0500

    2nd final

--- index.js
+++ index.js
@@ -31,7 +31,22 @@ y = 250;
 if(direction === 'west'){
     x = x - 1
 }
-
+function move(element) { element.style.position = 'fixed'; function moveToCoordinates(left, bottom) 
+{ element.style.left = left + 'px'; element.style.bottom = bottom + 'px' } 
+function moveWithArrowKeys(left, bottom, callback)
+{ 
+    let direction = null; let x = left; let y = bottom; element.style.left = x + 'px'; element.style.bottom = y + 'px'; function moveCharacter()
+    { if(direction === 'west'){ x-=1 } if(direction === 'north'){ y+=1 } if(direction === 'east'){ x+=1 } if(direction === 'south'){ y-=1 } 
+    element.style.left = x + 'px'; element.style.bottom = y + 'px' } setInterval(moveCharacter, 1) 
+    document.addEventListener('keydown', function(e){ if(e.repeat) return; if(e.key === 'ArrowLeft')
+    { direction = 'west' } if(e.key === 'ArrowUp')
+    { direction = 'north' } if(e.key === 'ArrowRight')
+    { direction = 'east' } 
+    if(e.key === 'ArrowDown')
+    { direction = 'south' } 
+    callback(direction) }) 
+    document.addEventListener('keyup', function(e){ direction = null; callback(direction) }) } 
+    return { to: moveToCoordinates, withArrowKeys: moveWithArrowKeys } }
 
 character = newImage('assets/green-character/static.gif')
 direction = null;
